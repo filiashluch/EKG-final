a044969fe0fbd9db699f13d149e45ce34321850f
filiashluch <55384004+filiashluch@users.noreply.github.com>
2020-11-09 12:39:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,116 @@
-#include <Arduino.h>
-#include <WiFi.h>
+ #include <WiFi.h> 
 #include <WebServer.h>
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <dht.h>  
-float teplota, vlhkost, tep, kyslik, teplota_tela;
+#include <dht.h>
  
-dht DHT;
+#define DHTTYPE DHT11
+#define DHT11_PIN 18
+#define DS18B20 5
+#define REPORTING_PERIOD_MS     1000
+ 
+float teplota, Vlhkost, BPM, KyslikKrev, TeplotaTela;
+ 
+
+const char* ssid = "filias";
+const char* password = "123456789";
+ 
+DHT dht;
 PulseOximeter pox;
-
-void setup() {
+uint32_t tsLastReport = 0;
+OneWire oneWire(DS18B20);
+DallasTemperature sensors(&oneWire);
+ 
+ 
+WebServer server(80);             
+ 
+void onBeatDetected()
+{
+  Serial.println("Tep detekovan");
 }
-
+ 
+void setup() {
+  Serial.begin(115200);
+  pinMode(19, OUTPUT);
+  delay(100);   
+ 
+  Serial.println("Pripojuji se ");
+  Serial.println(ssid);
+ 
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, password);
+ 
+  //check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi pripojena");
+  Serial.print("IP: ");  Serial.println(WiFi.localIP());
+ 
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+ 
+  server.begin();
+  Serial.println("HTTP server start");
+ 
+  Serial.print("Inicialnizace detektoru pulzu/kysliky");
+ 
+  if (!pox.begin()) {
+    Serial.println("ERROR");
+    for (;;);
+  } else {
+    Serial.println("SUCCESS");
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+  }
+ 
+   pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+ 
+ 
+}
 void loop() {
+  server.handleClient();
+  pox.update();
+  sensors.requestTemperatures();
+  int chk = DHT.read11(DHT11_PIN);
+  
+  teplota = dht.Teplota;
+  vlhkost = dht.Vlhkost;
+  BPM = pox.getHeartRate();
+  kyslikKrev = pox.getSpO2();
+  teplotaTela = sensors.getTempCByIndex(0);
+ 
+  
+  if (millis() - tsLastReport > REPORTING_PERIOD_MS) 
+  {
+    Serial.print("Teplota v mistnosti: ");
+    Serial.print(dht.Teplota);
+    Serial.println("°C");
+    
+    Serial.print("Vlhkost v mistnosti: ");
+    Serial.print(dht.Vlhkost);
+    Serial.println("%");
+    
+    Serial.print("BPM: ");
+    Serial.println(BPM);
+    
+    Serial.print("Kyslik v krvi: ");
+    Serial.print(KyslikKrev);
+    Serial.println("%");
+ 
+    Serial.print("Teplota tela: ");
+    Serial.print(TeplotaTela);
+    Serial.println("°C");
+    
+    Serial.println("*********************************");
+    Serial.println();
+ 
+    tsLastReport = millis();
+  }
+  
 }
 
  String SendHTML(float teplota,float vlhkost,float tep,float kyslik, float teplota_tela){
